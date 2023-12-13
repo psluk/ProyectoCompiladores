e04c8885015ebd31a4cc6310239bdb285f1d4f2a
Paúl Rodríguez García <70735615+psluk@users.noreply.github.com>
2023-12-13 14:28:23 -0600

    Add missing terminals, generateParserLexer

--- ProyectoCompi/src/App.java
+++ ProyectoCompi/src/App.java
@@ -2,25 +2,28 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class App {
-    public static void generateLexerParser()throws Exception{
+    public static void generateLexerParser() throws Exception {
         String basePath, fullPathLexer, fullPathParser, jlexer, jparser, jlexerFolder;
         MainJflexCup mfjc = new MainJflexCup();
 
         basePath = System.getProperty("user.dir");
         jlexer = "Lexer.java";
-        jparser = "Parser.java";
+        jparser = "parser.java";
         jlexerFolder = "SourceParserLexer";
 
-        Files.deleteIfExists(Paths.get(basePath+"\\src\\ParserLexer\\"+jlexer));
-        Files.deleteIfExists(Paths.get(basePath+"\\src\\ParserLexer\\"+jparser));
+        Files.deleteIfExists(Paths.get(basePath + "\\src\\ParserLexer\\" + jlexer));
+        Files.deleteIfExists(Paths.get(basePath + "\\src\\ParserLexer\\" + jparser));
+        Files.deleteIfExists(Paths.get(basePath + "\\src\\ParserLexer\\sym.java"));
 
-        fullPathLexer = basePath+"\\src\\SourceParserLexer\\SourceLexer.jflex";        
-        fullPathParser = basePath+"\\src\\SourceParserLexer\\SourceParser.cup"; 
-        
-        mfjc.generateLexer(fullPathLexer);
+        fullPathLexer = basePath + "\\src\\SourceParserLexer\\SourceLexer.jflex";
+        fullPathParser = basePath + "\\src\\SourceParserLexer\\SourceParser.cup";
 
-        //Files.move (Paths.get(basePath+"\\"+jparser), Paths.get(basePath+"\\src\\ParserLexer\\"+jparser));
-        Files.move (Paths.get(basePath+"\\src\\"+jlexerFolder+"\\"+jlexer), Paths.get(basePath+"\\src\\ParserLexer\\"+jlexer));
+        mfjc.generateParserLexer(fullPathLexer, fullPathParser);
+
+        Files.move(Paths.get(basePath + "\\sym.java"), Paths.get(basePath + "\\src\\ParserLexer\\sym.java"));
+        Files.move(Paths.get(basePath + "\\" + jparser), Paths.get(basePath + "\\src\\ParserLexer\\" + jparser));
+        Files.move(Paths.get(basePath + "\\src\\" + jlexerFolder + "\\" + jlexer),
+                Paths.get(basePath + "\\src\\ParserLexer\\" + jlexer));
     }
 
     public static void main(String[] args) throws Exception {
--- ProyectoCompi/src/MainJflexCup.java
+++ ProyectoCompi/src/MainJflexCup.java
@@ -2,19 +2,36 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-
+import java_cup.internal_error;
 import jflex.exceptions.SilentExit;
+import ParserLexer.*;
 
 public class MainJflexCup {
 
-    public void generateLexer(String ruta) throws IOException, SilentExit{
-        String[] strArr = {ruta};
+    public void generateLexer(String path) throws IOException, SilentExit {
+        String[] strArr = { path };
         jflex.Main.generate(strArr);
     }
 
-    public void runTest(String inputPath) throws IOException{
+    public void generateParser(String path) throws internal_error, Exception, IOException {
+        String[] strArr = { path };
+        java_cup.Main.main(strArr);
+    }
+
+    public void generateParserLexer(String lexerPath, String parserPath) throws Exception {
+        generateLexer(lexerPath);
+        generateParser(parserPath);
+    }
+
+    public void runTest(String inputPath) throws IOException {
         Reader reader = new BufferedReader(new FileReader(inputPath));
         reader.read();
+
+        parser parser;
+
+        // TODO: // Here would be the code to run the lexer
+
+        reader.close();
     }
-    
+
 }
